13e47959e22bff485881af23b99a815dd644e2cd
Akgop <skadbsckd0902@gmail.com>
2020-10-09 14:40:57 +0900

    Update six_tab_interpolation.c

--- six_tab_interpolation.c
+++ six_tab_interpolation.c
@@ -201,7 +201,7 @@ void Six_Tab_Filter(UCHAR img[512][512]) {
 			img[i][j + 3] = (img[i][j] + 3 * img[i][j + 4]) / 4;
 		}
 	}
-	for (int i = 500; i <= 508; i += 4) {
+	for (int i = 500; i < 508; i += 4) {
 		for (int j = 0; j <= 8; j++) {
 			img[i + 1][j] = (3 * img[i][j] + img[i + 4][j]) / 4;
 			img[i + 2][j] = (img[i][j] + img[i + 4][j]) / 2;
@@ -211,13 +211,13 @@ void Six_Tab_Filter(UCHAR img[512][512]) {
 	
 	// Boundary Bottom Right
 	for (int i = 0; i <= 8; i += 4) {
-		for (int j = 500; j <= 508; j += 4) {
+		for (int j = 500; j < 508; j += 4) {
 			img[i][j + 1] = (3 * img[i][j] + img[i][j + 4]) / 4;
 			img[i][j + 2] = (img[i][j] + img[i][j + 4]) / 2;
 			img[i][j + 3] = (img[i][j] + 3 * img[i][j + 4]) / 4;
 		}
 	}
-	for (int i = 0; i <= 8; i += 4) {
+	for (int i = 0; i < 8; i += 4) {
 		for (int j = 500; j <= 508; j++) {
 			img[i + 1][j] = (3 * img[i][j] + img[i + 4][j]) / 4;
 			img[i + 2][j] = (img[i][j] + img[i + 4][j]) / 2;
@@ -225,22 +225,109 @@ void Six_Tab_Filter(UCHAR img[512][512]) {
 		}
 	}
 
-	//오른쪽 아래 채우기
+	for (int i = 500; i < 508; i += 4) {
+		for (int j = 0; j < 492; j += 4) {
+			half_pel[0] = (img[i][j] - 5 * img[i][j + 4]
+				+ 20 * img[i][j + 8] + 20 * img[i][j + 12]
+				- 5 * img[i][j + 16] + img[i][j + 20]);
+			half_pel[1] = (img[i + 4][j] - 5 * img[i + 4][j + 4]
+				+ 20 * img[i + 4][j + 8] + 20 * img[i][j + 12]
+				- 5 * img[i + 4][j + 16] + img[i + 4][j + 20]);
+			half_pel[2] = (16 * img[i + 4][j + 8] + 20 * img[i][j + 8]
+				- 5 * img[i - 4][j + 8] + img[i - 8][j + 8]);
+			half_pel[3] = (16 * img[i + 4][j + 12] + 20 * img[i][j + 12]
+				- 5 * img[i - 4][j + 12] + img[i - 8][j + 12]);
+			center = (16 * half_pel[1] + 20 * half_pel[0]
+				- 5 * 32 * img[i - 4][j + 10] + 32 * img[i - 8][j + 10]);
 
+			//half_pel
+			img[i][j + 10] = half_pel[0] / 32;
+			img[i + 4][j + 10] = half_pel[1] / 32;
+			img[i + 2][j + 8] = half_pel[2] / 32;
+			img[i + 2][j + 12] = half_pel[3] / 32;
+			img[i + 2][j + 10] = center / 1024;
+
+			//quarter_pel
+			img[i][j + 9] = (32 * img[i][j + 8] + half_pel[0]) / 64;
+			img[i][j + 11] = (32 * img[i][j + 12] + half_pel[0]) / 64;
+			img[i + 1][j + 8] = (32 * img[i][j + 8] + half_pel[2]) / 64;
+			img[i + 3][j + 8] = (32 * img[i + 4][j + 8] + half_pel[2]) / 64;
+
+			img[i + 1][j + 12] = (32 * img[i][j + 12] + half_pel[3]) / 64;
+			img[i + 3][j + 12] = (32 * img[i + 4][j + 12] + half_pel[3]) / 64;
+			img[i + 4][j + 9] = (32 * img[i + 4][j + 8] + half_pel[1]) / 64;
+			img[i + 4][j + 11] = (32 * img[i + 4][j + 12] + half_pel[1]) / 64;
+
+			img[i + 1][j + 10] = (32 * half_pel[0] + center) / 2048;
+			img[i + 3][j + 10] = (32 * half_pel[1] + center) / 2048;
+			img[i + 2][j + 9] = (32 * half_pel[2] + center) / 2048;
+			img[i + 2][j + 11] = (32 * half_pel[3] + center) / 2048;
+
+			img[i + 1][j + 9] = (half_pel[0] + half_pel[2]) / 64;
+			img[i + 1][j + 11] = (half_pel[0] + half_pel[3]) / 64;
+			img[i + 3][j + 9] = (half_pel[1] + half_pel[2]) / 64;
+			img[i + 3][j + 11] = (half_pel[1] + half_pel[3]) / 64;
+
+			half_pel[4] = (img[j][i] - 5 * img[j + 4][i]
+				+ 20 * img[j + 8][i] + 20 * img[j + 12][i]
+				- 5 * img[j + 16][i] + img[j + 20][i]);
+			half_pel[5] = (img[j][i + 4] - 5 * img[j + 4][i + 4]
+				+ 20 * img[j + 8][i + 4] + 20 * img[j + 12][i + 4]
+				- 5 * img[j + 16][i + 4] + img[j + 20][i + 4]);
+			half_pel[6] = (16 * img[j + 8][i + 4] + 20 * img[j + 8][i]
+				- 5 * img[j + 8][i - 4] + img[j + 8][i - 8]);
+			half_pel[7] = (16 * img[j + 12][i + 4] + 20 * img[j + 12][i]
+				- 5 * img[j + 12][i - 4] + img[j + 12][i - 8]);
+			center = (16 * half_pel[5] + 20 * half_pel[6]
+				- 5 * 32 * img[j + 10][i - 4] + 32 * img[j + 10][i - 8]);
+
+			//half_pel
+			img[j + 10][i] = half_pel[4] / 32;
+			img[j + 10][i + 4] = half_pel[5] / 32;
+			img[j + 8][i + 2] = half_pel[6] / 32;
+			img[j + 12][i + 2] = half_pel[7] / 32;
+			img[j + 10][i + 2] = center / 1024;
+
+			//quarter_pel
+			img[j + 8][i + 1] = (32 * img[j + 8][i] + half_pel[6]) / 64;
+			img[j + 8][i + 3] = (32 * img[j + 8][i + 4] + half_pel[6]) / 64;
+			img[j + 9][i] = (32 * img[j + 8][i] + half_pel[4]) / 64;
+			img[j + 11][i] = (32 * img[j + 12][i] + half_pel[4]) / 64;
+			img[j + 9][i + 4] = (32 * img[j + 8][i + 4] + half_pel[5]) / 64;
+			img[j + 11][i + 4] = (32 * img[j + 12][i + 4] + half_pel[5]) / 64;
+			img[j + 12][i + 1] = (32 * img[j + 12][i] + half_pel[7]) / 64;
+			img[j + 12][i + 3] = (32 * img[j + 12][i + 4] + half_pel[7]) / 64;
+
+			img[j + 9][i + 2] = (32 * half_pel[6] + center) / 2048;
+			img[j + 11][i + 2] = (32 * half_pel[7] + center) / 2048;
+			img[j + 10][i + 1] = (32 * half_pel[4] + center) / 2048;
+			img[j + 10][i + 3] = (32 * half_pel[5] + center) / 2048;
+
+			img[j + 9][i + 1] = (half_pel[6] + half_pel[4]) / 64;
+			img[j + 9][i + 3] = (half_pel[6] + half_pel[5]) / 64;
+			img[j + 11][i + 1] = (half_pel[7] + half_pel[4]) / 64;
+			img[j + 11][i + 3] = (half_pel[7] + half_pel[5]) / 64;
+		}
+	}
 
 	for (int i = 500; i <= 508; i += 4) {
-		for (int j = 500; j <= 508; j += 4) {
+		for (int j = 500; j < 508; j += 4) {
 			img[i][j + 1] = (3 * img[i][j] + img[i][j + 4]) / 4;
 			img[i][j + 2] = (img[i][j] + img[i][j + 4]) / 2;
 			img[i][j + 3] = (img[i][j] + 3 * img[i][j + 4]) / 4;
 		}
 	}
-	for (int i = 500; i <= 508; i += 4) {
+	for (int i = 500; i < 508; i += 4) {
 		for (int j = 500; j <= 508; j++) {
 			img[i + 1][j] = (3 * img[i][j] + img[i + 4][j]) / 4;
 			img[i + 2][j] = (img[i][j] + img[i + 4][j]) / 2;
 			img[i + 3][j] = (img[i][j] + 3 * img[i + 4][j]) / 4;
 		}
+	}
+
+	// Boundary Outside
+	for (int i = 0; i < 512; i++) {
+
 	}
 }
 
